7027a59c0b4171be93f65ae9b5c1fe8219180507
MohtSing12 <mohitdelhi0987@gmail.com>
2024-10-14 22:21:21 +0530

    my commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+mongoose.connect('mongodb+srv://Mohitsing12:8273687989m@cluster0.ec7q9.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" }
+
+).then(()=>console.log("Database connected"));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
